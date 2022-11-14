34ae9ee3dd14bb66c4037b83b2c29212856eb649
Hinesh Miyani <hineshmiyani@gmail.com>
2022-11-15 00:32:10 +0530

    Design Fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,9 @@
 import React from "react";
-import Link from "next/link";
-import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
-import { useStateContext } from "../context/StateContext";
 
-const Footer = () => {
-  const { footerLinks } = useStateContext();
-
-  return (
-    <div className="footer-container">
-      <p>{new Date().getFullYear()} Envy Beats. All rights reserved</p>
-      <p className="icons">
-        {footerLinks?.[0]?.instagram && (
-          <Link href={footerLinks?.[0]?.instagram}>
-            <AiFillInstagram />
-          </Link>
-        )}
-        {footerLinks?.[0]?.twitter && (
-          <Link href={footerLinks?.[0]?.twitter}>
-            <AiOutlineTwitter />
-          </Link>
-        )}
-      </p>
-    </div>
-  );
-};
+const Footer = () => (
+  <div className="footer-container">
+    <p>{new Date().getFullYear()} Envy Beats. All rights reserved</p>
+  </div>
+);
 
 export default Footer;
